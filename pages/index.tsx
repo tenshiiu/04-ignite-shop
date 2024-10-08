6a419468666e9669d1a1113ba8728fc2b3132060
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-10-08 14:44:04 -0300

    Configurando Stitches

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,39 @@
+import { styled } from "@/styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+  padding: "4px, 8px",
+  border: 0,
+  borderRadius: 8,
+
+  span: {
+    fontWeight: "bold",
+  },
+
+  "&:hover": {
+    filter: "brightness(0.8)"
+  },
+})
+
+const Header = styled("header", {
+  display: "block",
+  backgroundColor: "Blue",
+  width: 256,
+  height: 100,
+  paddingBottom: 20,
+})
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+  <>
+    <Header>
+    Olá, boa tarde!
+    </Header>
+    <Button>
+    <span>Teste</span>    
+    Enviar
+    </Button>
+  </>
   )
 }
+
