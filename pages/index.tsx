d4f0582abbace12adab68d54485fee55012112e0
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-10-09 15:20:56 -0300

    Criando carrossel

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,20 @@ import camiseta1 from "../assets/shirts/1.png";
 import camiseta2 from "../assets/shirts/2.png";
 import camiseta3 from "../assets/shirts/3.png";
 
+import "keen-slider/keen-slider.min.css";
+import { useKeenSlider } from "keen-slider/react";
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+  slides: {
+    perView: 3,
+    spacing: 48
+  }    
+    
+  })
   return (
-  <HomeContainer>
-    <Product>
+  <HomeContainer ref={sliderRef} className="keen-slider">
+    <Product className="keen-slider__slide">
       <Image src={camiseta1} width={520} height={480} alt="" />
 
       <footer>
@@ -17,7 +27,7 @@ export default function Home() {
       </footer>
     </Product>
 
-    <Product>
+    <Product className="keen-slider__slide">
       <Image src={camiseta2} width={520} height={480} alt="" />
 
       <footer>
@@ -26,7 +36,7 @@ export default function Home() {
       </footer>
     </Product>
 
-    <Product>
+    <Product className="keen-slider__slide">
       <Image src={camiseta3} width={520} height={480} alt="" />
 
       <footer>
