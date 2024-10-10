d9c2e162cf4528aa222b3e6aeb78d40c8d926755
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-10-10 17:00:02 -0300

    Estrutura da página de produto

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,9 +1,23 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/product";
 import { useRouter } from "next/router";
 
 export default function Product() {
     const { query } = useRouter()
 
     return (
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,90</span>
+
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque aliquam ullam dicta commodi voluptatibus animi totam vero deleniti. Iure necessitatibus quidem magni enim recusandae ipsum voluptates maiores, laborum quisquam voluptatem.</p>
+
+                <button>Comprar agora</button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
